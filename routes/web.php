e5420e84f845b39797041b444358277866b8fcb4
xunandtek <72821027+xunandtek@users.noreply.github.com>
2026-01-19 00:30:18 -0800

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CountryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => redirect('/countries'));
+
+Route::get('/countries', [CountryController::class, 'index'])->name('countries.index');
+Route::get('/countries/{code}', [CountryController::class, 'show'])->name('countries.show');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
